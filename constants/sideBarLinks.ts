74a11da6172a8ec909c0a067dd8bb6ecdc2166c3
sammanamgain <amgain02@gmail.com>
2024-07-01 18:42:59 +0545

    add sidebar constants

---
+++ constants/sideBarLinks.ts
@@ -0,0 +1,27 @@
+export const sideBarLinks = [
+  {
+    imgUrl: "/icons/Home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgUrl: "/icons/upcoming.svg",
+    route: "/upcoming",
+    label: "upcoming",
+  },
+  {
+    imgUrl: "/icons/previous.svg",
+    route: "/previous",
+    label: "Previous",
+  },
+  {
+    imgUrl: "/icons/Video.svg",
+    route: "/recordings",
+    label: "Recordings",
+  },
+  {
+    imgUrl: "/icons/add-personal.svg",
+    route: "/personal-room",
+    label: "Personal Room",
+  },
+];
